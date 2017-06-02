fe4bb3e6ed67dd35e26bdc266676dd1edcd9b97c
Dominik Moritz <domoritz@gmail.com>
2017-06-01 21:48:36 -0700

    Refine Definition type

--- src/Schema/Definition.ts
+++ src/Schema/Definition.ts
@@ -1,3 +1,27 @@
+export type PrimitiveType = number | boolean | string | null;
+
 export interface Definition {
-    [key: string]: any;
-}
+    $ref?: string;
+    description?: string;
+    allOf?: Definition[];
+    oneOf?: Definition[];
+    anyOf?: Definition[];
+    title?: string;
+    type?: string | string[];
+    definitions?: {[key: string]: any};
+    format?: string;
+    items?: Definition;
+    minItems?: number;
+    additionalItems?: {
+        anyOf: Definition,
+    };
+    enum?: PrimitiveType[] | Definition[];
+    default?: PrimitiveType | Object;
+    additionalProperties?: false | Definition;
+    required?: string[];
+    propertyOrder?: string[];
+    properties?: {};
+    defaultProperties?: string[];
+
+    typeof?: "function";
+};
