ebc0e37e57c1b45b2df0384009019f1ba87bfbff
Dominik Moritz <domoritz@gmail.com>
2017-06-01 21:48:08 -0700

    Don’t try to resolve references to types that are not supported

--- src/NodeParser/TypeReferenceNodeParser.ts
+++ src/NodeParser/TypeReferenceNodeParser.ts
@@ -4,6 +4,11 @@ import { SubNodeParser } from "../SubNodeParser";
 import { BaseType } from "../Type/BaseType";
 import { ArrayType } from "../Type/ArrayType";
 
+const invlidTypes: {[index: number]: boolean} = {
+    [ts.SyntaxKind.ModuleDeclaration]: true,
+    [ts.SyntaxKind.VariableDeclaration]: true,
+};
+
 export class TypeReferenceNodeParser implements SubNodeParser {
     public constructor(
         private typeChecker: ts.TypeChecker,
@@ -19,7 +24,7 @@ export class TypeReferenceNodeParser implements SubNodeParser {
         if (typeSymbol.flags & ts.SymbolFlags.Alias) {
             const aliasedSymbol: ts.Symbol = this.typeChecker.getAliasedSymbol(typeSymbol);
             return this.childNodeParser.createType(
-                aliasedSymbol.declarations[0],
+                aliasedSymbol.declarations.filter((n: ts.Declaration) => !invlidTypes[n.kind])[0],
                 this.createSubContext(node, context),
             );
         } else if (typeSymbol.flags & ts.SymbolFlags.TypeParameter) {
@@ -28,7 +33,7 @@ export class TypeReferenceNodeParser implements SubNodeParser {
             return new ArrayType(this.createSubContext(node, context).getArguments()[0]);
         } else {
             return this.childNodeParser.createType(
-                typeSymbol.declarations[0],
+                typeSymbol.declarations.filter((n: ts.Declaration) => !invlidTypes[n.kind])[0],
                 this.createSubContext(node, context),
             );
         }
