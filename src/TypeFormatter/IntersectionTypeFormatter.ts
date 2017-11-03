85513ba6e4efc839af60d2d1bcf63fcd241c7c79
Dominik Moritz <domoritz@gmail.com>
2017-11-02 18:39:31 -0700

    Support objects in type intersections and still don’t generate definitions that are not needed.

--- src/TypeFormatter/IntersectionTypeFormatter.ts
+++ src/TypeFormatter/IntersectionTypeFormatter.ts
@@ -2,6 +2,7 @@ import { Definition } from "../Schema/Definition";
 import { SubTypeFormatter } from "../SubTypeFormatter";
 import { BaseType } from "../Type/BaseType";
 import { IntersectionType } from "../Type/IntersectionType";
+import { ObjectType } from "../Type/ObjectType";
 import { TypeFormatter } from "../TypeFormatter";
 import { getAllOfDefinitionReducer } from "../Utils/allOfDefinition";
 
@@ -20,9 +21,15 @@ export class IntersectionTypeFormatter implements SubTypeFormatter {
             {type: "object", additionalProperties: false} as Definition);
     }
     public getChildren(type: IntersectionType): BaseType[] {
-        return type.getTypes().reduce((result: BaseType[], item: BaseType) => [
-            ...result,
-            ...this.childTypeFormatter.getChildren(item).slice(1),
-        ], []);
+        return type.getTypes().reduce((result: BaseType[], item: BaseType) => {
+            // Remove the first child, which is the definition of the child itself because we are merging objects.
+            // However, if the child is just a reference, we cannot remove it.
+            const slice = item instanceof ObjectType ? 0 : 1;
+            return [
+                ...result,
+                ...this.childTypeFormatter.getChildren(item).slice(slice),
+            ];
+        }
+        , []);
     }
 }
