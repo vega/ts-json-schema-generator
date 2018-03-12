55a985c59724b0f287fd5444773aaee77b1ae0b7
renewooller <rene>
2018-03-12 16:33:02 +1000

    adds fix for mapped type to support partial types

--- src/NodeParser/MappedTypeNodeParser.ts
+++ src/NodeParser/MappedTypeNodeParser.ts
@@ -24,23 +24,34 @@ export class MappedTypeNodeParser implements SubNodeParser {
         );
     }
 
-    private getObjectProperty(type: any, node: ts.MappedTypeNode, context: Context) {
-        return new ObjectProperty(
-            type.value,
-            this.childNodeParser.createType(node.type!, context),
-            !node.questionToken,
-        );
-    }
-
     private getProperties(node: ts.MappedTypeNode, context: Context): ObjectProperty[] {
-        const type: any = this.typeChecker.getTypeFromTypeNode(node.typeParameter.constraint!);
+        const type: any = this.typeChecker.getTypeFromTypeNode((<any>node.typeParameter.constraint));
 
-        if (type.types) {
-            return type.types.map((t: any) => this.getObjectProperty(t, node, context));
-        } else if (type.intrinsicName !== "never") {
-            return [this.getObjectProperty(type, node, context)];
-        } else {
-            return [];
+        let toRet :ObjectProperty[] = [];
+        if(type.types) {
+            toRet = type.types.reduce((result: ObjectProperty[], t: any) => {
+                const createdType = this.childNodeParser.createType(node.type!, context)
+
+                const objectProperty = new ObjectProperty(
+                    t.value,
+                    createdType,
+                    !node.questionToken,
+                );
+
+                result.push(objectProperty);
+                return result;
+            }, [])
+        } else if (context.getArguments()){
+            toRet = context.getArguments().reduce((acc : ObjectProperty[], arg : any) => {
+                acc = acc.concat(arg.getProperties()
+                .map((objProp : any) => {
+                    objProp.required = !node.questionToken
+                    return objProp;
+                }))
+                return acc;
+            }, [])
         }
+
+        return toRet;
     }
 }
