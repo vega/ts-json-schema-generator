0dc38ec4d6a4532466ecfe1d7488fc0209389a6e
renewooller <rene>
2018-03-14 16:36:33 +1000

    formats the valid-data-test.ts

--- test/valid-data.test.ts
+++ test/valid-data.test.ts
@@ -51,72 +51,72 @@ function assertSchema(name: string, type: string, only: boolean = false): void {
 }
 
 describe("valid-data", () => {
-     // TODO: generics recursive
+    // TODO: generics recursive
 
-        assertSchema("simple-object", "SimpleObject");
+    assertSchema("simple-object", "SimpleObject");
 
-        assertSchema("interface-single", "MyObject");
-        assertSchema("interface-multi", "MyObject");
-        assertSchema("interface-recursion", "MyObject");
-        assertSchema("interface-extra-props", "MyObject");
+    assertSchema("interface-single", "MyObject");
+    assertSchema("interface-multi", "MyObject");
+    assertSchema("interface-recursion", "MyObject");
+    assertSchema("interface-extra-props", "MyObject");
 
-        assertSchema("structure-private", "MyObject");
-        assertSchema("structure-anonymous", "MyObject");
-        assertSchema("structure-recursion", "MyObject");
-        assertSchema("structure-extra-props", "MyObject");
+    assertSchema("structure-private", "MyObject");
+    assertSchema("structure-anonymous", "MyObject");
+    assertSchema("structure-recursion", "MyObject");
+    assertSchema("structure-extra-props", "MyObject");
 
-        assertSchema("enums-string", "Enum");
-        assertSchema("enums-number", "Enum");
-        assertSchema("enums-initialized", "Enum");
-        assertSchema("enums-compute", "Enum");
-        assertSchema("enums-mixed", "Enum");
-        assertSchema("enums-member", "MyObject");
+    assertSchema("enums-string", "Enum");
+    assertSchema("enums-number", "Enum");
+    assertSchema("enums-initialized", "Enum");
+    assertSchema("enums-compute", "Enum");
+    assertSchema("enums-mixed", "Enum");
+    assertSchema("enums-member", "MyObject");
 
-        assertSchema("string-literals", "MyObject");
-        assertSchema("string-literals-inline", "MyObject");
-        assertSchema("string-literals-null", "MyObject");
+    assertSchema("string-literals", "MyObject");
+    assertSchema("string-literals-inline", "MyObject");
+    assertSchema("string-literals-null", "MyObject");
 
-        assertSchema("namespace-deep-1", "RootNamespace.Def");
-        assertSchema("namespace-deep-2", "RootNamespace.SubNamespace.HelperA");
-        assertSchema("namespace-deep-3", "RootNamespace.SubNamespace.HelperB");
+    assertSchema("namespace-deep-1", "RootNamespace.Def");
+    assertSchema("namespace-deep-2", "RootNamespace.SubNamespace.HelperA");
+    assertSchema("namespace-deep-3", "RootNamespace.SubNamespace.HelperB");
 
-        assertSchema("import-simple", "MyObject");
-        assertSchema("import-exposed", "MyObject");
-        assertSchema("import-anonymous", "MyObject");
+    assertSchema("import-simple", "MyObject");
+    assertSchema("import-exposed", "MyObject");
+    assertSchema("import-anonymous", "MyObject");
 
-        assertSchema("type-aliases-primitive", "MyString");
-        assertSchema("type-aliases-object", "MyAlias");
-        assertSchema("type-aliases-mixed", "MyObject");
-        assertSchema("type-aliases-union", "MyUnion");
-        assertSchema("type-aliases-union-array", "MyUnion");
-        assertSchema("type-aliases-tuple", "MyTuple");
-        assertSchema("type-aliases-anonymous", "MyObject");
-        assertSchema("type-aliases-local-namespace", "MyObject");
-        assertSchema("type-aliases-recursive-anonymous", "MyAlias");
-        assertSchema("type-aliases-recursive-export", "MyObject");
+    assertSchema("type-aliases-primitive", "MyString");
+    assertSchema("type-aliases-object", "MyAlias");
+    assertSchema("type-aliases-mixed", "MyObject");
+    assertSchema("type-aliases-union", "MyUnion");
+    assertSchema("type-aliases-union-array", "MyUnion");
+    assertSchema("type-aliases-tuple", "MyTuple");
+    assertSchema("type-aliases-anonymous", "MyObject");
+    assertSchema("type-aliases-local-namespace", "MyObject");
+    assertSchema("type-aliases-recursive-anonymous", "MyAlias");
+    assertSchema("type-aliases-recursive-export", "MyObject");
 
-        assertSchema("type-maps", "MyObject");
-        assertSchema("type-primitives", "MyObject");
-        assertSchema("type-union", "TypeUnion");
-        assertSchema("type-union-tagged", "Shape");
-        assertSchema("type-intersection", "MyObject");
-        assertSchema("type-intersection-additional-props", "MyObject");
+    assertSchema("type-maps", "MyObject");
+    assertSchema("type-primitives", "MyObject");
+    assertSchema("type-union", "TypeUnion");
+    assertSchema("type-union-tagged", "Shape");
+    assertSchema("type-intersection", "MyObject");
+    assertSchema("type-intersection-additional-props", "MyObject");
 
-        assertSchema("type-typeof", "MyType");
-        assertSchema("type-typeof-value", "MyType");
-        assertSchema("type-indexed-access", "MyType");
-        assertSchema("type-keyof", "MyType");
-        assertSchema("type-mapped", "MyObject");
-        assertSchema("type-partial", "MyObject");
-        assertSchema("type-pick", "MyObject");
-        assertSchema("generic-simple", "MyObject");
-        assertSchema("generic-arrays", "MyObject");
-        assertSchema("generic-multiple", "MyObject");
-        assertSchema("generic-multiargs", "MyObject");
-        assertSchema("generic-anonymous", "MyObject");
-        assertSchema("generic-recursive", "MyObject");
-        assertSchema("generic-hell", "MyObject");
+    assertSchema("type-typeof", "MyType");
+    assertSchema("type-typeof-value", "MyType");
+    assertSchema("type-indexed-access", "MyType");
+    assertSchema("type-keyof", "MyType");
+    assertSchema("type-mapped", "MyObject");
+    assertSchema("type-partial", "MyObject");
+    assertSchema("type-pick", "MyObject");
+    assertSchema("generic-simple", "MyObject");
+    assertSchema("generic-arrays", "MyObject");
+    assertSchema("generic-multiple", "MyObject");
+    assertSchema("generic-multiargs", "MyObject");
+    assertSchema("generic-anonymous", "MyObject");
+    assertSchema("generic-recursive", "MyObject");
+    assertSchema("generic-hell", "MyObject");
 
-        assertSchema("nullable-null", "MyObject");
+    assertSchema("nullable-null", "MyObject");
 
 });
