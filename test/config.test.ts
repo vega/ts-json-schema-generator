51d2c06fdfe5c837159dc6016c9867da1c80e5d8
Marco Q <marcoq@gmail.com>
2018-06-08 18:23:58 +0200

    linting

--- test/config.test.ts
+++ test/config.test.ts
@@ -62,6 +62,18 @@ describe("config", () => {
     assertSchema("jsdoc-hide", {type: "MyObject", expose: "export", topRef: true, jsDoc: "extended"});
     assertSchema("jsdoc-inheritance", {type: "MyObject", expose: "export", topRef: true, jsDoc: "extended"});
 
-    assertSchema("strict-tuples-true", {type: "MyObject", expose: "export", topRef: true, jsDoc: "none", strictTuples: true});
-    assertSchema("strict-tuples-false", {type: "MyObject", expose: "export", topRef: true, jsDoc: "none", strictTuples: false});
+    assertSchema("strict-tuples-true", {
+        type: "MyObject",
+        expose: "export",
+        topRef: true,
+        jsDoc: "none",
+        strictTuples: true,
+    });
+    assertSchema("strict-tuples-false", {
+        type: "MyObject",
+        expose: "export",
+        topRef: true,
+        jsDoc: "none",
+        strictTuples: false,
+    });
 });
