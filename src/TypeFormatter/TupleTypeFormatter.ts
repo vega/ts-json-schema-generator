604998f6b931cdcf3362757204a550d5e7fb982b
Marco Q <marcoq@gmail.com>
2018-08-02 15:55:37 +0200

    update tuple formatter for optional and rest elements + empty tuples

--- src/TypeFormatter/TupleTypeFormatter.ts
+++ src/TypeFormatter/TupleTypeFormatter.ts
@@ -2,6 +2,8 @@ import { Config } from "../Config";
 import { Definition } from "../Schema/Definition";
 import { SubTypeFormatter } from "../SubTypeFormatter";
 import { BaseType } from "../Type/BaseType";
+import { OptionalType } from "../Type/OptionalType";
+import { RestType } from "../Type/RestType";
 import { TupleType } from "../Type/TupleType";
 import { TypeFormatter } from "../TypeFormatter";
 
@@ -16,14 +18,27 @@ export class TupleTypeFormatter implements SubTypeFormatter {
         return type instanceof TupleType;
     }
     public getDefinition(type: TupleType): Definition {
-        const tupleDefinitions = type.getTypes().map((item) => this.childTypeFormatter.getDefinition(item));
-        const addAdditionalItems = tupleDefinitions.length > 1 && !this.config.strictTuples;
-        const additionalItems = {additionalItems: {anyOf: tupleDefinitions}};
+        const subTypes = type.getTypes();
+
+        const requiredElements = subTypes.filter(t => !(t instanceof OptionalType) && !(t instanceof RestType));
+        const optionalElements  = subTypes.filter(t => t instanceof OptionalType) as OptionalType[];
+        const restElements = subTypes.filter(t => t instanceof RestType) as RestType[];
+
+        const requiredDefinitions = requiredElements.map((item) => this.childTypeFormatter.getDefinition(item));
+        const optionalDefinitions = optionalElements.map((item) => this.childTypeFormatter.getDefinition(item));
+        const itemsTotal = requiredDefinitions.length + optionalDefinitions.length;
+
+        const restType = restElements.length ? restElements[0].getType().getItem() : undefined;
+        const restDefinition = restType ? this.childTypeFormatter.getDefinition(restType) : undefined;
+
         return {
             type: "array",
-            items: tupleDefinitions,
-            minItems: tupleDefinitions.length,
-            ...(addAdditionalItems ? additionalItems : {maxItems: tupleDefinitions.length}),
+            minItems: requiredDefinitions.length,
+            ...(itemsTotal ? { items: requiredDefinitions.concat(optionalDefinitions) } : {}), // with items
+            ...(!itemsTotal && restDefinition ? { items: restDefinition } : {}), // with only rest param
+            ...(!itemsTotal && !restDefinition ? { maxItems: 0 } : {}), // empty
+            ...(restDefinition && itemsTotal ? { additionalItems: restDefinition } : {}), // with items and rest
+            ...(!restDefinition && itemsTotal ? { maxItems: itemsTotal } : {}), // without rest
         };
     }
     public getChildren(type: TupleType): BaseType[] {
