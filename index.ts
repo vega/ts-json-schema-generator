4c388d8bbeb647a593e60065ca6c811e1b636f6d
Cyrus <4041334+Cyrus-d@users.noreply.github.com>
2019-02-25 22:33:26 +0000

    Factory export (#76)
    * export factory modules
    
    * clean code

--- index.ts
+++ index.ts
@@ -95,3 +95,5 @@ export * from "./src/NodeParser/AnnotatedNodeParser";
 export * from "./src/NodeParser/CallExpressionParser";
 
 export * from "./src/SchemaGenerator";
+
+export * from "./factory";
