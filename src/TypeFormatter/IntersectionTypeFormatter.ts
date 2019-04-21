52f0304d740325b71a3989ad7b01d0cfb1b6d5d0
Dominik Moritz <domoritz@gmail.com>
2019-04-20 20:58:37 -0700

    Fix children of merged objects

--- src/TypeFormatter/IntersectionTypeFormatter.ts
+++ src/TypeFormatter/IntersectionTypeFormatter.ts
@@ -27,11 +27,14 @@ export class IntersectionTypeFormatter implements SubTypeFormatter {
             : this.childTypeFormatter.getDefinition(types[0]);
     }
     public getChildren(type: IntersectionType): BaseType[] {
-        // ignore children that are objects as they get merged
-        const types = type.getTypes().filter(t => t instanceof ObjectType);
-        return types.reduce((result: BaseType[], item) => [
-            ...result,
-            ...this.childTypeFormatter.getChildren(item),
-        ], []);
+        return type.getTypes().reduce((result: BaseType[], item) => {
+            // Remove the first child, which is the definition of the child itself because we are merging objects.
+            // However, if the child is just a reference, we cannot remove it.
+            const slice = item instanceof ObjectType ? 0 : 1;
+            return [
+                ...result,
+                ...this.childTypeFormatter.getChildren(item).slice(slice),
+            ];
+        }, []);
     }
 }
