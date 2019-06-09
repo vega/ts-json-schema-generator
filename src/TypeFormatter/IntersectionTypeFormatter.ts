1e761a8a8ac0a5457bc5a38ef06876327a9321ef
Klaus Reimer <k.reimer@iplabs.de>
2019-06-09 14:24:13 +0200

    Remove accidentally commited comment

--- src/TypeFormatter/IntersectionTypeFormatter.ts
+++ src/TypeFormatter/IntersectionTypeFormatter.ts
@@ -32,7 +32,6 @@ export class IntersectionTypeFormatter implements SubTypeFormatter {
             // Remove the first child, which is the definition of the child itself because we are merging objects.
             // However, if the child is just a reference, we cannot remove it.
             const slice = item instanceof ObjectType ? 0 : 1;
-//            const slice = item instanceof DefinitionType ? 1 : 0;
             return [
                 ...result,
                 ...this.childTypeFormatter.getChildren(item).slice(slice),
