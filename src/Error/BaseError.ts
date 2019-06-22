8cdd6cc91cf17c0a29b2d6943ad30e775ba568ac
Klaus Reimer <k@ailis.de>
2019-06-22 17:58:37 +0200

    Improve exceptions (#141)
    Project is target ES2017 anyway so Error can be extended directly instead of faking error objects. As a result exceptions thrown in tests now yield the correct error message and stack trace

--- src/Error/BaseError.ts
+++ src/Error/BaseError.ts
@@ -1,14 +1,5 @@
-export abstract class BaseError implements Error {
-    private callStack: any;
-
-    public constructor() {
-        this.callStack = new Error().stack;
+export abstract class BaseError extends Error {
+    public constructor(message?: string) {
+        super(message);
     }
-
-    public get stack(): string {
-        return this.callStack;
-    }
-
-    public abstract get name(): string;
-    public abstract get message(): string;
 }
