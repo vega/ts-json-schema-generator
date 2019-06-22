3b154cf94b7fa9a5eb1a854816b789a91fe42514
Klaus Reimer <k.reimer@iplabs.de>
2019-06-22 12:16:31 +0200

    Improve exceptions
    Project is target ES2017 anyway so Error can be extended directly instead of faking error objects. As a result exceptions thrown in tests now yield the correct error message and stack trace

--- src/Error/LogicError.ts
+++ src/Error/LogicError.ts
@@ -2,13 +2,6 @@ import { BaseError } from "./BaseError";
 
 export class LogicError extends BaseError {
     public constructor(private msg: string) {
-        super();
-    }
-
-    public get name(): string {
-        return "LogicError";
-    }
-    public get message(): string {
-        return this.msg;
+        super(msg);
     }
 }
