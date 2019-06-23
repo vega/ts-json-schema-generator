3941f0df441f205940e9da1b5ad4d21af7c9d5c3
Andreas Huber <ahd6974-githubcom@outlook.com>
2019-06-23 19:20:08 +0200

    Make tests pass
    (the produced schema for MyAsset is incorrect, see comment)

--- test/valid-data/multiple-roots2/schema/MyAsset.ts
+++ test/valid-data/multiple-roots2/schema/MyAsset.ts
@@ -1,3 +1,35 @@
 import { Asset } from "../Asset";
 
+// TODO: The schema currently produced for this is wrong, it is expected to look more like the following:
+// {
+//     "$schema": "http://json-schema.org/draft-07/schema#",
+//     "definitions": {
+//         "Asset": {
+//             "type": "object",
+//             "properties": {
+//                 "type": {
+//                     "enum": [
+//                         "One",
+//                         "Two"
+//                     ],
+//                     "type": "string"
+//                 }
+//             },
+//             "required": [
+//                 "type"
+//             ],
+//             "additionalProperties": false
+//         },
+//         "AssetType": {
+//             "enum": [
+//                 "One",
+//                 "Two"
+//             ],
+//             "type": "string"
+//         },
+//         "MyAsset": {
+//             "$ref": "#/definitions/Asset"
+//         }
+//     }
+// }
 export type MyAsset = Asset;
