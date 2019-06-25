e86ddd0cbb56354633a744c8a3a51e57672cc2ba
Klaus Reimer <k.reimer@iplabs.de>
2019-06-25 10:00:35 +0200

    Fix handling enum declarations in typeof parser

--- src/NodeParser/TypeofNodeParser.ts
+++ src/NodeParser/TypeofNodeParser.ts
@@ -3,6 +3,10 @@ import { LogicError } from "../Error/LogicError";
 import { Context, NodeParser } from "../NodeParser";
 import { SubNodeParser } from "../SubNodeParser";
 import { BaseType } from "../Type/BaseType";
+import { ObjectType, ObjectProperty } from "../Type/ObjectType";
+import { ReferenceType } from "../Type/ReferenceType";
+import { getKey } from "../Utils/nodeKey";
+import { LiteralType } from "../Type/LiteralType";
 
 export class TypeofNodeParser implements SubNodeParser {
     public constructor(
@@ -15,17 +19,44 @@ export class TypeofNodeParser implements SubNodeParser {
         return node.kind === ts.SyntaxKind.TypeQuery;
     }
 
-    public createType(node: ts.TypeQueryNode, context: Context): BaseType {
+    public createType(node: ts.TypeQueryNode, context: Context, reference?: ReferenceType): BaseType {
         const symbol = this.typeChecker.getSymbolAtLocation(node.exprName)!;
 
-        const valueDec = symbol.valueDeclaration as any;
-
-        if (valueDec.type) {
-            return this.childNodeParser.createType(valueDec.type, context);
-        } else if (valueDec.initializer) {
-            return this.childNodeParser.createType(valueDec.initializer, context);
-        } else {
-            throw new LogicError(`Invalid type query "${valueDec.getFullText()}"`);
+        const valueDec = symbol.valueDeclaration;
+        if (ts.isEnumDeclaration(valueDec)) {
+            return this.createObjectFromEnum(valueDec, context, reference);
+        } else if (ts.isVariableDeclaration(valueDec) || ts.isPropertySignature(valueDec)) {
+            if (valueDec.type) {
+                return this.childNodeParser.createType(valueDec.type, context);
+            } else if (valueDec.initializer) {
+                return this.childNodeParser.createType(valueDec.initializer, context);
+            }
         }
+        throw new LogicError(`Invalid type query "${valueDec.getFullText()}" (ts.SyntaxKind = ${valueDec.kind})`);
+    }
+
+    private createObjectFromEnum(node: ts.EnumDeclaration, context: Context, reference?: ReferenceType): ObjectType {
+        const id = `typeof-enum-${getKey(node, context)}`
+        if (reference) {
+            reference.setId(id);
+            reference.setName(id);
+        }
+
+        let type: BaseType | null = null;
+        const properties = node.members.map(member => {
+            const name = member.name.getText();
+            if (member.initializer) {
+                type = this.childNodeParser.createType(member.initializer, context);
+            } else if (type === null) {
+                type = new LiteralType(0);
+            } else if (type instanceof LiteralType && typeof (type.getValue() === "number")) {
+                type = new LiteralType(+type.getValue() + 1);
+            } else {
+                throw new LogicError(`Enum initializer missing for "${name}"`);
+            }
+            return new ObjectProperty(name, type, true);
+        });
+
+        return new ObjectType(id, [], properties, false);
     }
 }
