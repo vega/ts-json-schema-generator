486dc93c9ba38f9e2ae7d29b458a8b5dca228e74
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-07-22 12:40:29 +0100

    Add enum index signature support

--- src/NodeParser/MappedTypeNodeParser.ts
+++ src/NodeParser/MappedTypeNodeParser.ts
@@ -11,10 +11,12 @@ import { StringType } from "../Type/StringType";
 import { UnionType } from "../Type/UnionType";
 import { derefType } from "../Utils/derefType";
 import { getKey } from "../Utils/nodeKey";
+import { EnumType, EnumValue } from '../Type/EnumType';
 
 export class MappedTypeNodeParser implements SubNodeParser {
     public constructor(
         private childNodeParser: NodeParser,
+        private typeChecker: ts.TypeChecker,
     ) {
     }
 
@@ -40,6 +42,8 @@ export class MappedTypeNodeParser implements SubNodeParser {
         } else if (keyListType instanceof NumberType) {
             return new ArrayType(this.childNodeParser.createType(node.type!,
                 this.createSubContext(node, keyListType, context)));
+        } else if (keyListType instanceof EnumType) {
+            return new ObjectType(id, [], this.getValues(node, keyListType, context), false);
         } else {
             throw new LogicError(
                 // eslint-disable-next-line max-len
@@ -61,6 +65,14 @@ export class MappedTypeNodeParser implements SubNodeParser {
                 result.push(objectProperty);
                 return result;
             }, []);
+    }
+
+    private getValues(node: ts.MappedTypeNode, keyListType: EnumType, context: Context): ObjectProperty[] {
+        return keyListType.getValues()
+            .filter((value: EnumValue) => !!value)
+            .map((value: EnumValue) =>
+                new ObjectProperty(value!.toString(), new LiteralType(value!), !node.questionToken)
+            );
     }
 
     private getAdditionalProperties(node: ts.MappedTypeNode, keyListType: UnionType, context: Context):
