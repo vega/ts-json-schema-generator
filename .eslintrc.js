ff541b571f919bb530f8ebc38a4ded62ce568f4b
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-07-22 17:24:08 +0100

    Format with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,48 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier", "prettier/@typescript-eslint", "prettier/standard"],
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/prefer-for-of": "error",
-    "@typescript-eslint/no-require-imports": "error",
-    "@typescript-eslint/no-parameter-properties": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        vars: "all",
-        args: "none",
-        ignoreRestSiblings: true
-      }
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/standard",
     ],
-    "@typescript-eslint/no-object-literal-type-assertion": "off",
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/prefer-interface": "warn",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "max-len": ["error", { code: 120, tabWidth: 4 }],
-    "linebreak-style": ["error", "unix"],
-    "no-alert": "error",
-    "prefer-const": "error",
-    "no-return-assign": "error",
-    "no-useless-call": "error",
-    "no-shadow": "error",
-    "no-useless-concat": "error",
-    "no-undef": "off"
-  }
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/prefer-for-of": "error",
+        "@typescript-eslint/no-require-imports": "error",
+        "@typescript-eslint/no-parameter-properties": "off",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                vars: "all",
+                args: "none",
+                ignoreRestSiblings: true,
+            },
+        ],
+        "@typescript-eslint/no-object-literal-type-assertion": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/prefer-interface": "warn",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "max-len": ["error", { code: 120, tabWidth: 4 }],
+        "linebreak-style": ["error", "unix"],
+        "no-alert": "error",
+        "prefer-const": "error",
+        "no-return-assign": "error",
+        "no-useless-call": "error",
+        "no-shadow": "error",
+        "no-useless-concat": "error",
+        "no-undef": "off",
+    },
 };
