fb44ee9b42f663438b20fe3c907060ed8e85eda4
Dominik Moritz <domoritz@gmail.com>
2020-02-20 17:22:11 -0800

    Support hidden unions. Fixes #337 (#338)

--- src/NodeParser/UnionNodeParser.ts
+++ src/NodeParser/UnionNodeParser.ts
@@ -11,12 +11,20 @@ export class UnionNodeParser implements SubNodeParser {
     public supportsNode(node: ts.UnionTypeNode): boolean {
         return node.kind === ts.SyntaxKind.UnionType;
     }
-    public createType(node: ts.UnionTypeNode, context: Context): BaseType {
+
+    public createType(node: ts.UnionTypeNode, context: Context): BaseType | undefined {
         const types = node.types
             .map(subnode => {
                 return this.childNodeParser.createType(subnode, context);
             })
             .filter(notUndefined);
+
+        if (types.length === 1) {
+            return types[0];
+        } else if (types.length === 0) {
+            return undefined;
+        }
+
         return new UnionType(types);
     }
 }
