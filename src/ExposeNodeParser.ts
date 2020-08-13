4eb4c103e492f1a22fee0270e3bf742189bcdb3c
jeromeh <jerome.henaff@amdocs.com>
2020-08-13 16:04:56 +0200

    export support improvement: - search real export in sourceFile instead of the exportSymbol - keep fullyQualifiedName in the definitions

--- src/ExposeNodeParser.ts
+++ src/ExposeNodeParser.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+import * as fs from "fs";
 import * as ts from "typescript";
 import { Context } from "./NodeParser";
 import { SubNodeParser } from "./SubNodeParser";
@@ -32,20 +34,73 @@ export class ExposeNodeParser implements SubNodeParser {
     }
 
     private isExportNode(node: ts.Node): boolean {
-        if (this.expose === "all") {
-            return node.kind !== ts.SyntaxKind.TypeLiteral;
-        } else if (this.expose === "none") {
-            return false;
+        // search real export in sourceFile instead of the exportSymbol
+        const sourceFile = node.getSourceFile();
+        const moduleSymbol = this.typeChecker.getSymbolAtLocation(sourceFile);
+        if (moduleSymbol) {
+            const exportSymbols = this.typeChecker.getExportsOfModule(moduleSymbol);
+
+            return exportSymbols.some((exportSymbol) => {
+                const resolved = this.resolveAliasedSymbol(exportSymbol);
+                return resolved.declarations?.some((declaration) => {
+                    return declaration === node;
+                });
+            });
         }
 
-        const localSymbol: ts.Symbol = (node as any).localSymbol;
-        return localSymbol ? "exportSymbol" in localSymbol : false;
+        return false;
     }
+
     private getDefinitionName(node: ts.Node, context: Context): string {
         const symbol = symbolAtNode(node)!;
-        const fullName = this.typeChecker.getFullyQualifiedName(symbol).replace(/^".*"\./, "");
+        let fullName = this.typeChecker.getFullyQualifiedName(symbol);
+
+        // extract file path from fullName
+        const fileIdentifier = fullName.replace(/^("(.*)"\.)?.*$/, "$2");
+
+        // generate a lightweight file identifier
+        if (fileIdentifier.length > 0) {
+            // search module path
+            const sourceFile = node.getSourceFile();
+            const modulePath = this.searchModuleRoot(path.dirname(path.resolve(sourceFile.fileName)));
+            const relativePath = modulePath ? path.relative(path.dirname(modulePath), fileIdentifier) : fileIdentifier;
+
+            // replace slashes with . to avoid encoding path pollution
+            // replace " with ' to separate file identifier from symbol name to avoid encoding
+            const lightFileIdentifier = relativePath.replace(/(\/|\\)/g, ".");
+            fullName = fullName.replace(`"${fileIdentifier}"`, `'${lightFileIdentifier}'`);
+        }
+
         const argumentIds = context.getArguments().map((arg) => arg?.getName());
 
         return argumentIds.length ? `${fullName}<${argumentIds.join(",")}>` : fullName;
+    }
+
+    private resolveAliasedSymbol(symbol: ts.Symbol): ts.Symbol {
+        return symbol && ts.SymbolFlags.Alias & symbol.flags ? this.typeChecker.getAliasedSymbol(symbol) : symbol;
+    }
+
+    private searchModuleRoot(startFolderPath: string | string[]): string | undefined {
+        let pathArray: string[];
+        if (typeof startFolderPath === "string") {
+            pathArray = startFolderPath.split(path.sep);
+        } else {
+            pathArray = startFolderPath;
+        }
+
+        if (!pathArray.length) {
+            return undefined;
+        }
+        const dir = pathArray.join(path.sep);
+        try {
+            if (fs.existsSync(path.join(dir, "package.json"))) {
+                return dir;
+            }
+        } catch (e) {
+            // catch error
+        }
+
+        pathArray.pop();
+        return this.searchModuleRoot(pathArray);
     }
 }
