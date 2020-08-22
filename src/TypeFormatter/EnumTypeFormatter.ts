b75badea9929d5cd40905aa6149f2b3ebcd9236b
Jakub Jirutka <jakub@jirutka.cz>
2020-08-22 21:10:01 +0200

    refactor: make EnumTypeFormatter cleaner and more typecheck friendly (#493)
    ...and also add a comment explaining why it returns "const" for enums
    with a single value.
    
    This is a follow-up for 6681ef16910336e3595765124d412115d115c669.

--- src/TypeFormatter/EnumTypeFormatter.ts
+++ src/TypeFormatter/EnumTypeFormatter.ts
@@ -12,12 +12,14 @@ export class EnumTypeFormatter implements SubTypeFormatter {
     public getDefinition(type: EnumType): Definition {
         const values = uniqueArray(type.getValues());
         const types = uniqueArray(values.map(typeName));
-        const isSingle = values.length === 1;
 
-        return {
-            type: types.length === 1 ? types[0] : types,
-            [isSingle ? "const" : "enum"]: isSingle ? values[0] : values,
-        };
+        // NOTE: We want to use "const" when referencing an enum member.
+        // However, this formatter is used both for enum members and enum types,
+        // so the side effect is that an enum type that contains just a single
+        // value is represented as "const" too.
+        return values.length === 1
+            ? { type: types[0], const: values[0] }
+            : { type: types.length === 1 ? types[0] : types, enum: values };
     }
     public getChildren(type: EnumType): BaseType[] {
         return [];
