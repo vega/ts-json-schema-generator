4dd241ecf391af08a68822be711b1ef7da29b2f7
jeromeh <jerome.henaff@amdocs.com>
2020-08-26 13:38:42 +0200

    improve error log

--- src/Error/UnknownNodeError.ts
+++ src/Error/UnknownNodeError.ts
@@ -3,7 +3,11 @@ import { BaseError } from "./BaseError";
 
 export class UnknownNodeError extends BaseError {
     public constructor(private node: ts.Node, private reference?: ts.Node) {
-        super(`Unknown node of kind ${ts.SyntaxKind[node.kind.valueOf()]} "${node.getFullText()}`);
+        super(
+            `Unknown node of kind ${ts.SyntaxKind[node.kind.valueOf()]} in file ${
+                node.getSourceFile().fileName
+            }: "${node.getFullText()}`
+        );
     }
 
     public getNode(): ts.Node {
