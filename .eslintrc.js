669d4324f0a631b52dfbb8a8aab8f45a77664894
Dominik Moritz <domoritz@gmail.com>
2021-02-22 19:16:37 +0100

    chore: simplify lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "prettier"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
     env: {
         browser: true,
