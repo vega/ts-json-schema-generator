e4f32f63c324f28ccfd8fee57fd2fcd8b0086a1f
Cameron Yick <cameron.yick@gmail.com>
2021-07-15 20:58:53 -0400

    chore: configure publish workflow with auto shipit

---
+++ auto.config.ts
@@ -0,0 +1,18 @@
+import { AutoRc } from "auto";
+
+
+/** Auto configuration */
+export default function rc(): AutoRc {
+    return {
+        plugins: [
+            "npm",
+            "conventional-commits",
+            "first-time-contributor",
+            "released"
+        ],
+        // Follow the 2 branch deployment scheme
+        // https://intuit.github.io/auto/docs/generated/shipit#next-branch-default
+        baseBranch: "stable",        // latest "official" release
+        prereleaseBranches: ["main"] // latest changes (subject to breaking). next, alpha, beta, and other multi-feature test branches can all be added here.
+    };
+}
