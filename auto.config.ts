a1d6c68809d3efa146d79f2f9970f1ce9ad306fc
Cameron Yick <hydrosquall@users.noreply.github.com>
2021-10-11 12:19:43 -0400

    chore(release): use bot email for making auto commits

--- auto.config.ts
+++ auto.config.ts
@@ -13,6 +13,7 @@ export default function rc(): AutoRc {
         // Follow the 2 branch deployment scheme
         // https://intuit.github.io/auto/docs/generated/shipit#next-branch-default
         baseBranch: "stable",        // latest "official" release
-        prereleaseBranches: ["next"] // latest changes (subject to breaking). next, alpha, beta, and other multi-feature test branches can all be added here.
+        prereleaseBranches: ["next"], // latest changes (subject to breaking). next, alpha, beta, and other multi-feature test branches can all be added here.
+        author: "GitHub Actions Bot <vega-actions-bot@users.noreply.github.com>"
     };
 }
