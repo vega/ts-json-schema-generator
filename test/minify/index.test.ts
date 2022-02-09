51b152c49f2dc2c0f7c0978d8ca6e03aba1572af
arthurfiorette <arthur.fiorette@gmail.com>
2022-02-09 16:21:46 -0300

    tests: added tests

---
+++ test/minify/index.test.ts
@@ -0,0 +1,48 @@
+import { execSync } from "child_process";
+import path from "path";
+
+const SCHEMA_PATH = path.resolve(__dirname, "schema.ts");
+
+const EXPECTED = {
+    $ref: "#/definitions/Schema",
+    $schema: "http://json-schema.org/draft-07/schema#",
+    definitions: {
+        Schema: {
+            additionalProperties: false,
+            properties: {
+                a: {
+                    type: "number",
+                },
+            },
+            type: "object",
+        },
+    },
+};
+
+describe("Tests --minify output", () => {
+    it("With minify", () => {
+        const stdout = execSync(`ts-json-schema-generator -p '${SCHEMA_PATH}' --minify`).toString();
+
+        // Only a newline at the end
+        expect(stdout.split("\n").length).toBe(1);
+
+        // There's a newline at the end
+        expect(stdout.trimEnd()).toEqual(JSON.stringify(EXPECTED));
+
+        // The same output for both
+        expect(JSON.parse(stdout)).toStrictEqual(EXPECTED);
+    });
+
+    it("Without minify", () => {
+        const stdout = execSync(`ts-json-schema-generator -p '${SCHEMA_PATH}'`).toString();
+
+        // There's more than one \n (formatting)
+        expect(stdout.split("\n").length).toBeGreaterThan(2);
+
+        // There's a newline at the end
+        expect(stdout.trimEnd()).toEqual(JSON.stringify(EXPECTED, null, 2));
+
+        // The same output for both
+        expect(JSON.parse(stdout)).toStrictEqual(EXPECTED);
+    });
+});
