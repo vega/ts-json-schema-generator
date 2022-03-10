744274049599e5974f36f0ad48992b8f570a16c7
Hadrien Milano <hadrien.milano@datadoghq.com>
2022-03-10 11:38:45 +0100

    feat: add support for never type

---
+++ test/valid-data/never-record/main.ts
@@ -0,0 +1,3 @@
+// This form is recommended by the typescript-eslint rule "ban-types" to mean
+// "an object with no properties"
+export type Mapped = Record<string, never>;
---
+++ test/valid-data/never/main.ts
@@ -0,0 +1,1 @@
+export type BasicNever = never;
