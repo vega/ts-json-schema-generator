3d7a019f67dad2d81ed2fe65a52a831a5e0b7d3c
Hadrien Milano <hadrien.milano@datadoghq.com>
2022-04-07 09:50:54 +0200

    add other tags in test

--- test/valid-data/annotation-ref/main.ts
+++ test/valid-data/annotation-ref/main.ts
@@ -1,11 +1,17 @@
 
 export interface MyObject {
     /**
+     * Nested description
+     *
+     * @title Nested title
      * @ref http://json-schema.org/draft-07/schema#
      */
     nested: MyNestedObject
 
     /**
+     * MyObject description
+     *
+     * @title MyObject title
      * @ref http://json-schema.org/draft-07/schema#
      */
     myObject: { [key: string]: string };
