5cc790be76ced1e0dd92ec48315b5e765b2be883
Daan Boer <daan-boer@hotmail.com>
2022-05-25 16:58:11 +0200

    Added test cases for empty mapped types

--- test/valid-data-type.test.ts
+++ test/valid-data-type.test.ts
@@ -95,6 +95,8 @@ describe("valid-data-type", () => {
     it("type-mapped-exclude", assertValidSchema("type-mapped-exclude", "MyObject", "extended"));
     it("type-mapped-double-exclude", assertValidSchema("type-mapped-double-exclude", "MyObject", "extended"));
     it("type-mapped-symbol", assertValidSchema("type-mapped-symbol", "MyObject"));
+    it("type-mapped-never", assertValidSchema("type-mapped-never", "MyObject"));
+    it("type-mapped-empty-exclude", assertValidSchema("type-mapped-empty-exclude", "MyObject"));
     it("type-mapped-annotated-string", assertValidSchema("type-mapped-annotated-string", "ExchangeRate", "extended"));
 
     it("type-conditional-simple", assertValidSchema("type-conditional-simple", "MyObject"));
