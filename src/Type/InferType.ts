8cf3070a59a64a7d70304bf8efb32848f83ea5b4
Daan Boer <daan-boer@hotmail.com>
2022-05-27 14:07:15 +0200

    Cleanup InferType

--- src/Type/InferType.ts
+++ src/Type/InferType.ts
@@ -1,42 +1,11 @@
 import { BaseType } from "./BaseType";
 
 export class InferType extends BaseType {
-    // private id: string | null;
-    // private name: string | null;
-
     constructor(private id: string) {
         super();
     }
 
     public getId(): string {
-        if (this.id == null) {
-            throw new Error("Infer type ID not set yet");
-        }
         return this.id;
     }
-
-    // public setId(id: string) {
-    //     this.id = id;
-    // }
-
-    public getName(): string {
-        return this.getId();
-    }
-
-    // public setName(name: string) {
-    //     this.name = name;
-    // }
-
-    // public getType(): BaseType {
-    //     if (this.type == null) {
-    //         throw new Error("Infer type not set yet.");
-    //     }
-    //     return this.type;
-    // }
-
-    // public setType(type: BaseType): void {
-    //     this.type = type;
-    //     this.setId(type.getId());
-    //     this.setName(type.getName());
-    // }
 }
