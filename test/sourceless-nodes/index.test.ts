965cc8aba048f753b6669cabf0129f21d4fa75e4
arthurfiorette <arthur.fiorette@gmail.com>
2022-08-30 08:36:54 -0300

    test: sourceless nodes and  promise wrapping tests

---
+++ test/sourceless-nodes/index.test.ts
@@ -0,0 +1,53 @@
+import path from "path";
+import ts from "typescript";
+import { createParser } from "../../factory";
+import { Context } from "../../src/NodeParser";
+import { LiteralType } from "../../src/Type/LiteralType";
+import { NumberType } from "../../src/Type/NumberType";
+import { ObjectType } from "../../src/Type/ObjectType";
+
+const SOURCE = path.resolve(__dirname, "./source.ts");
+
+describe("sourceless-nodes", () => {
+    it("tests creating json schemas with ts.Nodes without valid source files", () => {
+        const program = ts.createProgram([SOURCE], {});
+        const parser = createParser(program, {});
+
+        // Finds the typescript function declaration node
+        const source = program.getSourceFile(SOURCE);
+        const fn = source!.statements[0] as ts.FunctionDeclaration;
+
+        // Creates a sourceless type by inferring the function return type.
+        const inferredReturnType = getReturnType(fn, program.getTypeChecker());
+
+        // Checks that the inferred return type does not have any real source file.
+        expect(inferredReturnType.getSourceFile()).toBeUndefined();
+
+        // Generates the json schema of this inferred return type
+        const baseType = parser.createType(inferredReturnType, new Context(inferredReturnType));
+
+        const objectType = (baseType as any).type as ObjectType;
+        expect(objectType).toBeDefined();
+        expect(objectType).toBeInstanceOf(ObjectType);
+
+        const [propA, propB] = objectType.getProperties();
+
+        expect(propA.getName()).toBe("a");
+        expect(propA.getType()).toBeInstanceOf(NumberType);
+
+        expect(propB.getName()).toBe("b");
+        expect(propB.getType()).toBeInstanceOf(LiteralType);
+    });
+});
+
+// github.com/arthurfiorette/kita/blob/main/packages/generator/src/util/type-resolver.ts
+function getReturnType(node: ts.SignatureDeclaration, typeChecker: ts.TypeChecker) {
+    if (node.type) {
+        return node.type;
+    }
+
+    const signature = typeChecker.getSignatureFromDeclaration(node);
+    const implicitType = typeChecker.getReturnTypeOfSignature(signature!);
+
+    return typeChecker.typeToTypeNode(implicitType, undefined, ts.NodeBuilderFlags.NoTruncation) as ts.TypeNode;
+}
