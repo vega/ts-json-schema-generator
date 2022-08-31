3124c34d2b056b596a8f18476f745caef7a64451
Tom Mrazauskas <tom@mrazauskas.de>
2022-08-31 17:26:10 +0300

    feat: bump `typescript` to 4.8.2 (#1388)

--- src/TypeFormatter/UnionTypeFormatter.ts
+++ src/TypeFormatter/UnionTypeFormatter.ts
@@ -56,7 +56,9 @@ export class UnionTypeFormatter implements SubTypeFormatter {
 
         // Flatten anyOf inside anyOf unless the anyOf has an annotation
         for (const def of definitions) {
-            if (Object.keys(def) === ["anyOf"]) {
+            const keys = Object.keys(def);
+
+            if (keys.length === 1 && keys[0] === "anyOf") {
                 flattenedDefinitions.push(...(def.anyOf as any));
             } else {
                 flattenedDefinitions.push(def);
