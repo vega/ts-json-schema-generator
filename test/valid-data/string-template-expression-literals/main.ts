a3ca92364e1c6cffc3caedb54fdbcc251d886a94
Alexander Tømmerås <flugged@gmail.com>
2022-12-20 17:51:52 +0100

    feat(tests): add failing tests for number types in template literal strings

--- test/valid-data/string-template-expression-literals/main.ts
+++ test/valid-data/string-template-expression-literals/main.ts
@@ -2,9 +2,11 @@ type OK = "ok";
 type Result = OK | "fail" | `abort`;
 type PrivateResultId = `__${Result}_id`;
 type OK_ID = `id_${OK}`;
+type Num = `${number}`;
 
 export interface MyObject {
     foo: Result;
     _foo: PrivateResultId;
     ok: OK_ID;
+    num: Num;
 }
