5326faee7e615dffbf89c07d7079c09276a56fcc
Alexander Tømmerås <flugged@gmail.com>
2022-12-21 11:38:38 +0100

    feat: add failing test for booleans in template strings

--- test/valid-data/string-template-expression-literals/main.ts
+++ test/valid-data/string-template-expression-literals/main.ts
@@ -3,10 +3,12 @@ type Result = OK | "fail" | `abort`;
 type PrivateResultId = `__${Result}_id`;
 type OK_ID = `id_${OK}`;
 type Num = `${number}`;
+type Bool = `${boolean}`;
 
 export interface MyObject {
     foo: Result;
     _foo: PrivateResultId;
     ok: OK_ID;
     num: Num;
+    bool: Bool;
 }
