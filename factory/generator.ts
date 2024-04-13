a643beed3b2c8e9638434e970df96e4bcb8ad8b6
Dominik Moritz <domoritz@gmail.com>
2024-04-13 13:25:32 -0400

    fix: support partial config in createGenerator
    fixes #1918

--- factory/generator.ts
+++ factory/generator.ts
@@ -1,13 +1,14 @@
-import { CompletedConfig } from "../src/Config";
+import { Config, DEFAULT_CONFIG } from "../src/Config";
 import { SchemaGenerator } from "../src/SchemaGenerator";
 import { createFormatter } from "./formatter";
 import { createParser } from "./parser";
 import { createProgram } from "./program";
 
-export function createGenerator(config: CompletedConfig): SchemaGenerator {
-    const program = createProgram(config);
-    const parser = createParser(program, config);
-    const formatter = createFormatter(config);
+export function createGenerator(config: Config): SchemaGenerator {
+    const completedConfig = { ...DEFAULT_CONFIG, ...config };
+    const program = createProgram(completedConfig);
+    const parser = createParser(program, completedConfig);
+    const formatter = createFormatter(completedConfig);
 
-    return new SchemaGenerator(program, parser, formatter, config);
+    return new SchemaGenerator(program, parser, formatter, completedConfig);
 }
