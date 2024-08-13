6bbd048c20a0a5d189142f355373791c1d72c986
Arthur Fiorette <me@arthur.place>
2024-08-13 13:07:26 -0300

    fix

--- test/valid-data-type.test.ts
+++ test/valid-data-type.test.ts
@@ -144,7 +144,7 @@ describe("valid-data-type", () => {
 
     it("lowercase", assertValidSchema("lowercase", "MyType"));
     it("const-spread", assertValidSchema("const-spread", "MyType"));
-    it.only("keyof-typeof-x", assertValidSchema("keyof-typeof-x", "MyType"));
+    it("keyof-typeof-x", assertValidSchema("keyof-typeof-x", "MyType"));
 
     it("promise-extensions", assertValidSchema("promise-extensions", "*"));
 
