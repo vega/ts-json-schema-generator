9d675d317e9b157192b29c0322250c8bb411428f
Arthur Fiorette <me@arthur.place>
2024-09-07 00:15:45 -0300

    code

--- src/NodeParser/BooleanTypeNodeParser.ts
+++ src/NodeParser/BooleanTypeNodeParser.ts
@@ -9,8 +9,6 @@ export class BooleanTypeNodeParser implements SubNodeParser {
         return node.kind === ts.SyntaxKind.BooleanKeyword;
     }
     public createType(node: ts.KeywordTypeNode, context: Context): BaseType {
-        let a = undefined;
-        a.toString();
         return new BooleanType();
     }
 }
