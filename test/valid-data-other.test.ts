6ae6379d4c92a79918d5b135281f0e5a8c940b58
Arthur Fiorette <me@arthur.place>
2025-01-07 16:00:41 -0300

    feat: added exported test cases

--- test/valid-data-other.test.ts
+++ test/valid-data-other.test.ts
@@ -10,6 +10,7 @@ describe("valid-data-other", () => {
     it("enums-member", assertValidSchema("enums-member", "MyObject"));
     it("enums-template-literal", assertValidSchema("enums-template-literal", "MyObject"));
     it("enums-union", assertValidSchema("enums-union", "MyObject"));
+    it("exported-enums-union", assertValidSchema("exported-enums-union", "MyObject"));
 
     it("function-parameters-default-value", assertValidSchema("function-parameters-default-value", "myFunction"));
     it("function-parameters-declaration", assertValidSchema("function-parameters-declaration", "myFunction"));
