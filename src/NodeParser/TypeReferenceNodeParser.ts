782ac8215fde6125c1d9ed715d3c605faeaedb09
Arthur Fiorette <me@arthur.place>
2025-01-27 00:33:07 -0300

    fix: weird bug

--- src/NodeParser/TypeReferenceNodeParser.ts
+++ src/NodeParser/TypeReferenceNodeParser.ts
@@ -6,7 +6,6 @@ import { AnyType } from "../Type/AnyType.js";
 import { ArrayType } from "../Type/ArrayType.js";
 import type { BaseType } from "../Type/BaseType.js";
 import { StringType } from "../Type/StringType.js";
-import { UnknownType } from "../Type/UnknownType.js";
 import { symbolAtNode } from "../Utils/symbolAtNode.js";
 
 const invalidTypes: Record<number, boolean> = {
@@ -46,7 +45,7 @@ export class TypeReferenceNodeParser implements SubNodeParser {
         }
 
         if (typeSymbol.flags & ts.SymbolFlags.TypeParameter) {
-            return context.getArgument(typeSymbol.name) || new UnknownType();
+            return context.getArgument(typeSymbol.name);
         }
 
         // Wraps promise type to avoid resolving to a empty Object type.
