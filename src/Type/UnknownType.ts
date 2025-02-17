f43d1f301ef8bc472ce0f9576104781f46f7351e
Arthur Fiorette <me@arthur.place>
2025-02-17 11:23:32 -0300

    fix

--- src/Type/UnknownType.ts
+++ src/Type/UnknownType.ts
@@ -5,7 +5,7 @@ export class UnknownType extends BaseType {
         /**
          * If the source for this UnknownType was from a failed operation than to an actual `unknown` type present in the source code.
          */
-        readonly erroredSource = false,
+        readonly erroredSource: boolean,
     ) {
         super();
     }
