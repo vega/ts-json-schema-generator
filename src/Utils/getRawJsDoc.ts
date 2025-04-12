f5f9b5ff652d2fc4a666addb8b46414bb8c4b24c
alexchexes <alexchexes@gmail.com>
2025-04-12 16:15:07 +0100

    refactor(getRawJsDoc): Simplify code

--- src/Utils/getRawJsDoc.ts
+++ src/Utils/getRawJsDoc.ts
@@ -1,4 +1,3 @@
-// Portions from ts-morph - https://github.com/dsherret/ts-morph (c) 2017 David Sherret, MIT
 import ts from "typescript";
 
 export function getRawJsDoc(node: ts.Node): string | undefined {
@@ -20,51 +19,21 @@ export function getRawJsDoc(node: ts.Node): string | undefined {
     return getTextWithoutStars(rawText).trim();
 }
 
-const regExWhitespaceSet = new Set(
-    [" ", "\f", "\n", "\r", "\t", "\v", "\u00A0", "\u2028", "\u2029"].map((c) => c.charCodeAt(0)),
-);
-
-const CharCodes = {
-    ASTERISK: "*".charCodeAt(0),
-    NEWLINE: "\n".charCodeAt(0),
-    CARRIAGE_RETURN: "\r".charCodeAt(0),
-    SPACE: " ".charCodeAt(0),
-    TAB: "\t".charCodeAt(0),
-    CLOSE_BRACE: "}".charCodeAt(0),
-};
-
-function isWhitespaceCharCode(charCode: number) {
-    return regExWhitespaceSet.has(charCode);
-}
-
-function getStarPosIfFirstNonWhitespaceChar(text: string) {
-    for (let i = 0; i < text.length; i++) {
-        const charCode = text.charCodeAt(i);
-
-        if (charCode === CharCodes.ASTERISK) {
-            return i;
-        } else if (!isWhitespaceCharCode(charCode)) {
-            break;
-        }
-    }
-    return -1;
-}
-
 function getTextWithoutStars(inputText: string) {
     const innerTextWithStars = inputText.replace(/^\/\*\*[^\S\n]*\n?/, "").replace(/(\r?\n)?[^\S\n]*\*\/$/, "");
 
     return innerTextWithStars
         .split(/\n/)
         .map((line) => {
-            const starPos = getStarPosIfFirstNonWhitespaceChar(line);
+            const trimmedLine = line.trimStart();
 
-            if (starPos === -1) {
+            if (trimmedLine[0] !== "*") {
                 return line;
             }
 
-            const substringStart = line[starPos + 1] === " " ? starPos + 2 : starPos + 1;
+            const textStartPos = trimmedLine[1] === " " ? 2 : 1;
 
-            return line.substring(substringStart);
+            return trimmedLine.substring(textStartPos);
         })
         .join("\n");
 }
