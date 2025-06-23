1622f18f974b54c85a43a2fe77bf59f449881596
alexchexes <alexchexes@gmail.com>
2025-06-23 22:27:06 +0100

    cleanup

--- test/valid-data-other.test.ts
+++ test/valid-data-other.test.ts
@@ -57,8 +57,8 @@ describe("valid-data-other", () => {
     it("generic-nested", assertValidSchema("generic-nested", "MyObject"));
     it("generic-prefixed-number", assertValidSchema("generic-prefixed-number", "MyObject"));
     it("generic-void", assertValidSchema("generic-void", "MyObject"));
-    it("generic-mapped-complex", assertValidSchema("generic-mapped-complex", "*", { encodeRefs: true }));
-    it("generic-valueof", assertValidSchema("generic-valueof", "*", { encodeRefs: true }));
+    it("generic-mapped-complex", assertValidSchema("generic-mapped-complex", "*"));
+    it("generic-valueof", assertValidSchema("generic-valueof", "*"));
 
     it("nullable-null", assertValidSchema("nullable-null", "MyObject"));
 
