daf1162c78283e545b18b3bf652f62b8b9b480e9
Sam Sudar <sudar.sam@gmail.com>
2025-11-25 08:32:05 -0800

    union types passing

--- test/valid-data/binary-expression/main.ts
+++ test/valid-data/binary-expression/main.ts
@@ -1,3 +1,7 @@
+type StringUnion = "a" | "b";
+type NumberUnion = 10 | 20;
+type MixedUnion = "c" | 30;
+
 const foo = {
     numbers: 60 * 5,
     strings: "a" + "b",
@@ -5,6 +9,11 @@ const foo = {
     any: 1 + ("test" as any),
     threeNumbers: 60 * 5 + 1,
     mixedStringAndNumbers: 60 * 5 + " minutes",
+    bigintType: BigInt(123),
+
+    stringUnion: ("a" as StringUnion) + ("b" as StringUnion),
+    numberUnion: (10 as NumberUnion) + (20 as NumberUnion),
+    mixedUnion: (30 as MixedUnion) + " is a number",
 } as const;
 
 export type MyObject = typeof foo;
