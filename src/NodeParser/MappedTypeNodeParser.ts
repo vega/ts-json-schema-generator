e5fea9f76dde5bbf1d48d069056cba3796c4cb2c
Sam Sudar <sudar.sam@gmail.com>
2025-11-24 20:50:17 -0800

    fix test

--- src/NodeParser/MappedTypeNodeParser.ts
+++ src/NodeParser/MappedTypeNodeParser.ts
@@ -4,6 +4,7 @@ import type { NodeParser } from "../NodeParser.js";
 import { Context } from "../NodeParser.js";
 import type { SubNodeParser } from "../SubNodeParser.js";
 import { AnnotatedType } from "../Type/AnnotatedType.js";
+import { AnyType } from "../Type/AnyType.js";
 import { ArrayType } from "../Type/ArrayType.js";
 import type { BaseType } from "../Type/BaseType.js";
 import { DefinitionType } from "../Type/DefinitionType.js";
@@ -55,7 +56,8 @@ export class MappedTypeNodeParser implements SubNodeParser {
         if (
             keyListType instanceof StringType ||
             keyListType instanceof NumberType ||
-            keyListType instanceof SymbolType
+            keyListType instanceof SymbolType ||
+            keyListType instanceof AnyType
         ) {
             if (constraintType?.getId() === "number") {
                 const type = this.childNodeParser.createType(
