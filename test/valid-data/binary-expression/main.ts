7f85f4de821da96b4e2207dcccd22c4a09fff2f4
Sam Sudar <sudar.sam@gmail.com>
2025-11-25 07:18:21 -0800

    switch to three nums

--- test/valid-data/binary-expression/main.ts
+++ test/valid-data/binary-expression/main.ts
@@ -1,5 +1,9 @@
 const foo = {
-    foo: 60 * 5,
+    numbers: 60 * 5,
+    threeNumbers: 60 * 5 + 1,
+    strings: "a" + "b",
+    booleans: true && false,
+    any: 1 + ("test" as any),
 } as const;
 
 export type MyObject = typeof foo;
