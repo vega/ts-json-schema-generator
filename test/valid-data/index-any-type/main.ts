31bec0e41cfe3632e98de5fd92555d50c78cdd00
Sam Sudar <sudar.sam@gmail.com>
2025-11-26 10:53:49 -0800

    add some tests

--- test/valid-data/index-any-type/main.ts
+++ test/valid-data/index-any-type/main.ts
@@ -1,5 +1,18 @@
+type AorB = "a" | "b";
+
+type MyObject = Record<any, number>;
+
 export interface MyObject {
-    nodes: {
+    anyNodes: {
         [Key in any]: number;
+    };
+    symbolNodes: {
+        [Key: symbol]: string;
+    };
+    numberNodes: {
+        [Key: number]: string;
+    };
+    templateLiteralNodes: {
+        [Key in `key${AorB}`]: number;
     };
 }
