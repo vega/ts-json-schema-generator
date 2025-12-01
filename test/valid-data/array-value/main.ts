5be9a5958eddd384d7504c18e3e7df5f768c6c22
Sam Sudar <sudar.sam@gmail.com>
2025-11-30 17:07:37 -0800

    simplify again

--- test/valid-data/array-value/main.ts
+++ test/valid-data/array-value/main.ts
@@ -1,10 +1,3 @@
-export type BasicObj = {
-    name: string;
-    age: number;
-};
-
 export type MyType = {
-    strToNumberArr: Record<string, number[]>;
-    numToNumberArr: Record<string, number[]>;
-    numToObjArr: Record<string, BasicObj[]>;
+    numToNumberArr: Record<number, number[]>;
 };
