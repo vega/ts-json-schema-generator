ab3fc77e2e36cf87b560bae45148029cee2f71a0
Sam Sudar <sudar.sam@gmail.com>
2025-12-01 07:15:03 -0800

    fix test with star

--- test/valid-data/type-mapped-number/index.test.ts
+++ test/valid-data/type-mapped-number/index.test.ts
@@ -1,4 +1,4 @@
 import { assertValidSchema } from "../../utils";
 import { test } from "node:test";
 
-test("type-mapped-number", assertValidSchema("type-mapped-number", "MyObject"));
+test("type-mapped-number", assertValidSchema("type-mapped-number", "*"));
