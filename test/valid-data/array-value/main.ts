1da2ff7d50efe14b657f29d08cfcb83dde1f62d4
Sam Sudar <sudar.sam@gmail.com>
2025-11-30 17:03:51 -0800

    tests in place but something funky

--- test/valid-data/array-value/main.ts
+++ test/valid-data/array-value/main.ts
@@ -1,3 +1,10 @@
+export type BasicObj = {
+    name: string;
+    age: number;
+};
+
 export type MyType = {
     strToNumberArr: Record<string, number[]>;
+    numToNumberArr: Record<string, number[]>;
+    numToObjArr: Record<string, BasicObj[]>;
 };
