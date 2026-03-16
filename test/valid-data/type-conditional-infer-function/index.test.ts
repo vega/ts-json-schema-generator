0bab491a7f6e0730e9a39580004f16f36873247e
Jackson Breit <84806918+ninjack-dev@users.noreply.github.com>
2026-03-16 09:39:44 -0700

    lint: format pass

--- test/valid-data/type-conditional-infer-function/index.test.ts
+++ test/valid-data/type-conditional-infer-function/index.test.ts
@@ -1,4 +1,7 @@
 import { assertValidSchema } from "../../utils";
 import { test } from "node:test";
 
-test("valid-data - type-conditional-infer-function", assertValidSchema("type-conditional-infer-function", "FuncParams"));
+test(
+    "valid-data - type-conditional-infer-function",
+    assertValidSchema("type-conditional-infer-function", "FuncParams"),
+);
