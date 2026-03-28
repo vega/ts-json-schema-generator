e32df4f3d88ae0110c9b99446de3ddc0885d9635
Arthur Fiorette <me@arthur.place>
2026-03-28 00:05:58 -0300

    format

--- test/valid-data/promise-extensions/main.ts
+++ test/valid-data/promise-extensions/main.ts
@@ -22,24 +22,18 @@ export default class extends Promise<A> {}
 export class LikeClass implements PromiseLike<A> {
     then<TResult1 = A, TResult2 = never>(
         onfulfilled?: ((value: A) => TResult1 | PromiseLike<TResult1>) | null | undefined,
-        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined
+        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined,
     ): PromiseLike<TResult1 | TResult2> {
         return new Promise(() => {});
     }
 }
 
 export abstract class LikeAbstractClass implements PromiseLike<A> {
-		abstract then<TResult1 = A, TResult2 = never>(
-			onfulfilled?:
-				| ((value: A) => TResult1 | PromiseLike<TResult1>)
-				| null
-				| undefined,
-			onrejected?:
-				| ((reason: unknown) => TResult2 | PromiseLike<TResult2>)
-				| null
-				| undefined,
-		): PromiseLike<TResult1 | TResult2>;
-	}
+    abstract then<TResult1 = A, TResult2 = never>(
+        onfulfilled?: ((value: A) => TResult1 | PromiseLike<TResult1>) | null | undefined,
+        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined,
+    ): PromiseLike<TResult1 | TResult2>;
+}
 
 export interface LikeInterface extends PromiseLike<A> {}
 
@@ -51,14 +45,14 @@ export interface WithProperty extends Promise<A> {
 export interface ThenableInterface {
     then<TResult1 = A, TResult2 = never>(
         onfulfilled?: ((value: A) => TResult1 | PromiseLike<TResult1>) | null | undefined,
-        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined
+        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined,
     ): PromiseLike<TResult1 | TResult2>;
 }
 
 export class ThenableClass {
     then<TResult1 = A, TResult2 = never>(
         onfulfilled?: ((value: A) => TResult1 | PromiseLike<TResult1>) | null | undefined,
-        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined
+        onrejected?: ((reason: unknown) => TResult2 | PromiseLike<TResult2>) | null | undefined,
     ): PromiseLike<TResult1 | TResult2> {
         return new Promise(() => {});
     }
